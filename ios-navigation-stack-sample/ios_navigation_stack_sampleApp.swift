ea0dc1d30a04e46d332cb104cdd7ae94ac1864a4
Matej Molnár <matej.molnar@outlook.com>
2022-10-05 15:29:43 +0200

    Initial Commit

---
+++ ios-navigation-stack-sample/ios_navigation_stack_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_navigation_stack_sampleApp.swift
+//  ios-navigation-stack-sample
+//
+//  Created by Matej Molnár on 05.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_navigation_stack_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
